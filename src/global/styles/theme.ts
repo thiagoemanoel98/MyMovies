f36f9916246375b849c1408ac8367ea30a4bfa15
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-12-27 21:25:59 -0300

    feat: fonts and colors global

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    primary: '#242A32',
+    secundary: '#0296E5',
+
+    shape: '#FFFFFF',
+    text1: '#EEEEEE',
+
+    orange: '#F1EE23',
+    black: '#000000',
+    white: '#fff',
+  },
+
+  fonts: {
+    light: 'Montserrat-Light',
+    regular: 'Montserrat-Regular',
+    medium: 'Montserrat-Medium',
+    bold: 'Montserrat-Bold',
+    semiBold: 'Montserrat-SemiBold',
+  },
+};
